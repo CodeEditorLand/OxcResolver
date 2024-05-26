a009eb778d3f629f2c83d9a98b88620c57942e71
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-26 14:19:46 +0300

    squash!

--- napi/wasi-worker-browser.mjs
+++ napi/wasi-worker-browser.mjs
@@ -1,40 +1,44 @@
-import { instantiateNapiModuleSync, MessageHandler, WASI } from '@napi-rs/wasm-runtime'
-import { Volume, createFsFromVolume } from '@napi-rs/wasm-runtime/fs'
+import {
+	MessageHandler,
+	WASI,
+	instantiateNapiModuleSync,
+} from "@napi-rs/wasm-runtime";
+import { Volume, createFsFromVolume } from "@napi-rs/wasm-runtime/fs";
 
 const fs = createFsFromVolume(
-  Volume.fromJSON({
-    '/': null,
-  }),
-)
+	Volume.fromJSON({
+		"/": null,
+	}),
+);
 
 const handler = new MessageHandler({
-  onLoad({ wasmModule, wasmMemory }) {
-    const wasi = new WASI({
-      fs,
-      print: function () {
-        // eslint-disable-next-line no-console
-        console.log.apply(console, arguments)
-      },
-      printErr: function() {
-        // eslint-disable-next-line no-console
-        console.error.apply(console, arguments)
-      },
-    })
-    return instantiateNapiModuleSync(wasmModule, {
-      childThread: true,
-      wasi,
-      overwriteImports(importObject) {
-        importObject.env = {
-          ...importObject.env,
-          ...importObject.napi,
-          ...importObject.emnapi,
-          memory: wasmMemory,
-        }
-      },
-    })
-  },
-})
+	onLoad({ wasmModule, wasmMemory }) {
+		const wasi = new WASI({
+			fs,
+			print: () => {
+				// eslint-disable-next-line no-console
+				console.log.apply(console, arguments);
+			},
+			printErr: () => {
+				// eslint-disable-next-line no-console
+				console.error.apply(console, arguments);
+			},
+		});
+		return instantiateNapiModuleSync(wasmModule, {
+			childThread: true,
+			wasi,
+			overwriteImports(importObject) {
+				importObject.env = {
+					...importObject.env,
+					...importObject.napi,
+					...importObject.emnapi,
+					memory: wasmMemory,
+				};
+			},
+		});
+	},
+});
 
-globalThis.onmessage = function (e) {
-  handler.handle(e)
-}
+globalThis.onmessage = (e) => {
+	handler.handle(e);
+};
--- napi/wasi-worker.mjs
+++ napi/wasi-worker.mjs
@@ -1,50 +1,54 @@
 import fs from "node:fs";
 import { createRequire } from "node:module";
-import { parentPort, Worker } from "node:worker_threads";
+import { Worker, parentPort } from "node:worker_threads";
 
-import { instantiateNapiModuleSync, MessageHandler, WASI } from "@napi-rs/wasm-runtime";
+import {
+	MessageHandler,
+	WASI,
+	instantiateNapiModuleSync,
+} from "@napi-rs/wasm-runtime";
 
 const require = createRequire(import.meta.url);
 
 if (parentPort) {
-  parentPort.on("message", (data) => {
-    globalThis.onmessage({ data });
-  });
+	parentPort.on("message", (data) => {
+		globalThis.onmessage({ data });
+	});
 }
 
 Object.assign(globalThis, {
-  self: globalThis,
-  require,
-  Worker,
-  importScripts: function (f) {
-    ;(0, eval)(fs.readFileSync(f, "utf8") + "//# sourceURL=" + f);
-  },
-  postMessage: function (msg) {
-    if (parentPort) {
-      parentPort.postMessage(msg);
-    }
-  },
+	self: globalThis,
+	require,
+	Worker,
+	importScripts: (f) => {
+		(0, eval)(fs.readFileSync(f, "utf8") + "//# sourceURL=" + f);
+	},
+	postMessage: (msg) => {
+		if (parentPort) {
+			parentPort.postMessage(msg);
+		}
+	},
 });
 
 const handler = new MessageHandler({
-  onLoad({ wasmModule, wasmMemory }) {
-    const wasi = new WASI({ fs });
+	onLoad({ wasmModule, wasmMemory }) {
+		const wasi = new WASI({ fs });
 
-    return instantiateNapiModuleSync(wasmModule, {
-      childThread: true,
-      wasi,
-      overwriteImports(importObject) {
-        importObject.env = {
-          ...importObject.env,
-          ...importObject.napi,
-          ...importObject.emnapi,
-          memory: wasmMemory
-        };
-      },
-    });
-  },
+		return instantiateNapiModuleSync(wasmModule, {
+			childThread: true,
+			wasi,
+			overwriteImports(importObject) {
+				importObject.env = {
+					...importObject.env,
+					...importObject.napi,
+					...importObject.emnapi,
+					memory: wasmMemory,
+				};
+			},
+		});
+	},
 });
 
-globalThis.onmessage = function (e) {
-  handler.handle(e);
+globalThis.onmessage = (e) => {
+	handler.handle(e);
 };
